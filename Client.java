f3bbfba086d26ac4a26dec42ba65bf9c9c84edf8
kanokwan <kanokwan.kum@dome.tu.ac.th>
2024-05-20 20:58:11 +0700

    addFuncCheckAnsWithBrackets

--- Client.java
+++ Client.java
@@ -22,7 +22,6 @@ public class Client extends Thread {
     public int time = 30;
     public boolean alreadyAnswered = false;
 
-
     public Client(int port, String serverAddress) {
         connect = false;
         try {
@@ -47,28 +46,26 @@ public class Client extends Thread {
             frame.setSize(base * 4, base * 3);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setLayout(new FlowLayout());
-    
+
             JLabel label = new JLabel("Enter Your Name");
             Font labelFont = label.getFont();
             label.setFont(new Font(labelFont.getName(), Font.PLAIN, 20));
-    
+
             JTextField textField = new JTextField(35);
-            textField.setSize(new Dimension(100,50));
+            textField.setSize(new Dimension(100, 50));
             Font textFieldFont = textField.getFont();
             textField.setFont(new Font(textFieldFont.getName(), Font.PLAIN, 20));
-    
+
             JButton EnterGame = new JButton("Enter Game");
-    
+
             frame.add(label);
             frame.add(textField);
             frame.add(EnterGame);
 
-            
             EnterGame.addActionListener(e -> {
-                
-                frame.getContentPane().removeAll(); 
+
+                frame.getContentPane().removeAll();
                 frame.setLayout(new GridLayout(3, 3));
-               
 
                 JLabel gameName = new JLabel("     Game 24");
                 Font gameNameFont = gameName.getFont();
@@ -86,18 +83,15 @@ public class Client extends Thread {
 
                 JLabel problem = new JLabel("Problem: ");
                 Font problemFont = problem.getFont();
-                    problem.setFont(new Font(problemFont.getName(), Font.PLAIN, 20));
+                problem.setFont(new Font(problemFont.getName(), Font.PLAIN, 20));
 
-                try{
+                try {
                     sendData("getProblem");
                     number = recvINTArrayData();
-                    problem.setText("Problem: " + number[0]  + " " + number[1] + " " + number[2] + " " + number[3]);
-                }
-                catch(IOException e2){
+                    problem.setText("Problem: " + number[0] + " " + number[1] + " " + number[2] + " " + number[3]);
+                } catch (IOException e2) {
                     System.out.println("Error in getProblem");
                 }
-
-             
 
                 JPanel sPanel = new JPanel();
                 sPanel.setLayout(new GridLayout(4, 1));
@@ -108,7 +102,7 @@ public class Client extends Thread {
 
                 JLabel score2 = new JLabel("");
                 score2.setFont(new Font(scoreFont.getName(), Font.PLAIN, 17));
-                
+
                 JLabel score3 = new JLabel("");
                 score3.setFont(new Font(scoreFont.getName(), Font.PLAIN, 17));
 
@@ -121,53 +115,53 @@ public class Client extends Thread {
                 sPanel.add(score4);
 
                 JLabel info = new JLabel("     Can Use +, -, *, / to get 24 only");
-                
+
                 JLabel timeLeftLabel = new JLabel("Time: 30");
                 Font timeLeftFont = timeLeftLabel.getFont();
                 timeLeftLabel.setFont(new Font(timeLeftFont.getName(), Font.PLAIN, 20));
-                
+
                 JLabel warning = new JLabel("");
                 Font warningFont = warning.getFont();
                 warning.setFont(new Font(warningFont.getName(), Font.PLAIN, 20));
-                                
+
                 JTextField input = new JTextField(35);
-                input.setSize(new Dimension(50,40));
-                
+                input.setSize(new Dimension(50, 40));
+
                 JButton submit = new JButton("Submit");
                 submit.setPreferredSize(new Dimension(100, 50));
 
                 Thread timer = new Thread() {
                     public void run() {
-                       
+
                         while (true) {
-                            try{
+                            try {
                                 sendData("time");
                                 time = recvINTData();
                                 timeLeftLabel.setText("Time: " + time);
-                            } catch (IOException e1){
+                            } catch (IOException e1) {
                                 System.out.println("Error in time");
                             }
-                            try{
+                            try {
                                 sendData("getProblem");
                                 number = recvINTArrayData();
-                                problem.setText("Problem: " + number[0]  + " " + number[1] + " " + number[2] + " " + number[3]);
-                                if(time == 0){
+                                problem.setText(
+                                        "Problem: " + number[0] + " " + number[1] + " " + number[2] + " " + number[3]);
+                                if (time == 0) {
                                     alreadyAnswered = false;
                                     warning.setText("");
                                 }
                                 Thread.sleep(1000);
-                            }
-                            catch(IOException | InterruptedException e2){
+                            } catch (IOException | InterruptedException e2) {
                                 System.out.println("Error in getProblem");
                             }
                         }
-                        
+
                     }
                 };
 
-                Thread getScore = new Thread(){
-                    public void run(){
-                        while (true){
+                Thread getScore = new Thread() {
+                    public void run() {
+                        while (true) {
                             try {
                                 sendData("score");
                             } catch (IOException e) {
@@ -180,15 +174,14 @@ public class Client extends Thread {
                                 score3.setText(data[2]);
                                 score4.setText(data[3]);
                                 Thread.sleep(1000);
-                                
+
                             } catch (IOException | InterruptedException e) {
                                 System.out.println("Error in t3");
                             }
                         }
-                       
+
                     }
                 };
-                
 
                 frame.add(gameName);
                 frame.add(playerNameLabel);
@@ -202,71 +195,89 @@ public class Client extends Thread {
                 timer.start();
                 getScore.start();
 
-
-                try{
-                    sendData("add,"+name);
-                } catch (IOException e1){
+                try {
+                    sendData("add," + name);
+                } catch (IOException e1) {
                     System.out.println("Error in add (sendData)");
                 }
-                
-                submit.addActionListener(e1 -> {   
+
+                submit.addActionListener(e1 -> {
                     String answer = input.getText();
                     boolean c = checkAns(answer);
-                    if(alreadyAnswered){
+                    if (alreadyAnswered) {
                         warning.setText("     Already answered");
-                    } else if(!c){
+                    } else if (!c) {
                         warning.setText("     Incorrect Answer");
                     }
-                    if(c){                   
+                    if (c) {
                         try {
-                            sendData("correct,"+name);
+                            sendData("correct," + name);
                         } catch (IOException e2) {
                             System.out.println("Error IN submit btn");
                         }
                         warning.setText("     Correct Answer");
                         alreadyAnswered = true;
+                        clearInputField(input);
                     }
-                    
+
                 });
 
                 frame.revalidate();
                 frame.repaint();
             });
-    
+
             frame.setVisible(true);
-            
+
         }
-        
+
     }
 
-    public boolean checkAns(String answer){
+    public static boolean checkBracket(String x) {
+        for (int i = 0; i < x.length(); i++) {
+            char c = x.charAt(i);
+            if (c == '(' || c == ')') {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean checkAns(String answer) {
         if (alreadyAnswered) {
             return false;
         }
-        if(answer.length() > 7){
+
+        answer = clearWSpace(answer);
+        if (checkBracket(answer)) {
+            return checkAnsWithBrackets(answer);
+        } else {
+            return checkSimpleAns(answer);
+        }
+    }
+
+    private boolean checkSimpleAns(String answer) {
+        if (answer.length() > 7) {
             return false;
         }
         clearCheckNumber();
         int sum = 0;
-        answer = answer.trim();
-        answer = clearWSpace(answer);
         int[] num = getNumber(answer);
         checkNumbers(number);
-        for(int i : num){
+        for (int i : num) {
             checkNumber[i]--;
             if (checkNumber[i] < 0) {
                 return false;
             }
         }
-        for(int i = 1; i < answer.length(); i = i+2){
+        for (int i = 1; i < answer.length(); i = i + 2) {
             char c = answer.charAt(i);
             if (c < '0' || c > '9') {
                 switch (c) {
                     case '+':
-                        if(i == 1){
+                        if (i == 1) {
                             sum = num[0] + num[1];
                         }
-                        if(i == 3){
+                        if (i == 3) {
                             sum = sum + num[2];
                         }
                         if (i == 5) {
@@ -274,10 +285,10 @@ public class Client extends Thread {
                         }
                         break;
                     case '-':
-                        if(i == 1){
+                        if (i == 1) {
                             sum = num[0] - num[1];
                         }
-                        if(i == 3){
+                        if (i == 3) {
                             sum = sum - num[2];
                         }
                         if (i == 5) {
@@ -285,10 +296,10 @@ public class Client extends Thread {
                         }
                         break;
                     case '*':
-                        if(i == 1){
+                        if (i == 1) {
                             sum = num[0] * num[1];
                         }
-                        if(i == 3){
+                        if (i == 3) {
                             sum = sum * num[2];
                         }
                         if (i == 5) {
@@ -296,10 +307,10 @@ public class Client extends Thread {
                         }
                         break;
                     case '/':
-                        if(i == 1){
+                        if (i == 1) {
                             sum = num[0] / num[1];
                         }
-                        if(i == 3){
+                        if (i == 3) {
                             sum = sum / num[2];
                         }
                         if (i == 5) {
@@ -311,18 +322,55 @@ public class Client extends Thread {
                 }
             }
         }
-               
+
         return sum == 24;
     }
 
-    private String clearWSpace(String str){
+    private boolean checkAnsWithBrackets(String answer) {
+        while (answer.contains("(")) {
+            int openIndex = answer.lastIndexOf('(');
+            int closeIndex = answer.indexOf(')', openIndex);
+            String subExpr = answer.substring(openIndex + 1, closeIndex);
+            int subResult = evaluateSimpleExpression(subExpr);
+            answer = answer.substring(0, openIndex) + subResult + answer.substring(closeIndex + 1);
+        }
+        return evaluateSimpleExpression(answer) == 24;
+    }
+
+    private int evaluateSimpleExpression(String expr) {
+        int[] num = getNumber(expr);
+        int sum = num[0];
+        for (int i = 1; i < expr.length(); i += 2) {
+            char op = expr.charAt(i);
+            int nextNum = num[i / 2 + 1];
+            switch (op) {
+                case '+':
+                    sum += nextNum;
+                    break;
+                case '-':
+                    sum -= nextNum;
+                    break;
+                case '*':
+                    sum *= nextNum;
+                    break;
+                case '/':
+                    sum /= nextNum;
+                    break;
+                default:
+                    break;
+            }
+        }
+        return sum;
+    }
+
+    private String clearWSpace(String str) {
         return str.replaceAll(" ", "");
     }
 
-    private int[] getNumber(String answer){
+    private int[] getNumber(String answer) {
         int[] res = new int[4];
         int index = 0;
-        for(int i = 0; i < answer.length(); i = i + 2){
+        for (int i = 0; i < answer.length(); i = i + 2) {
             char c = answer.charAt(i);
             int num = c - '0';
             res[index] = num;
@@ -331,20 +379,17 @@ public class Client extends Thread {
         return res;
     }
 
-
-    private void checkNumbers(int[] num){
-        for(int i = 0; i < num.length; i++){
+    private void checkNumbers(int[] num) {
+        for (int i = 0; i < num.length; i++) {
             checkNumber[num[i]]++;
         }
     }
 
-    private void clearCheckNumber(){
-        for(int i = 0; i < checkNumber.length; i++){
+    private void clearCheckNumber() {
+        for (int i = 0; i < checkNumber.length; i++) {
             checkNumber[i] = 0;
         }
     }
-
-    
 
     public void sendData(String data) throws IOException {
         if (out != null) {
@@ -356,9 +401,9 @@ public class Client extends Thread {
 
     public int[] recvINTArrayData() throws IOException {
         String receivedData = in.readLine();
-        
-        int[] res = null;           
-        String[] strArray = receivedData.substring(1, receivedData.length() - 1).split(", "); // Remove brackets and split by ", "
+
+        int[] res = null;
+        String[] strArray = receivedData.substring(1, receivedData.length() - 1).split(", ");
         res = new int[strArray.length];
         try {
             for (int i = 0; i < strArray.length; i++) {
@@ -383,5 +428,8 @@ public class Client extends Thread {
         return res;
     }
 
-}
+    public void clearInputField(JTextField inputField) {
+        inputField.setText("");
+    }
 
+}
